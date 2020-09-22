e2e465bd33cd18e45a19329509ba4562940ad64a
EC2 Default User <ec2-user@ip-172-31-12-93.us-east-2.compute.internal>
2020-09-22 19:40:36 +0000

    Mm

---
+++ resources/views/gdn/check2.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>公式ページへ移動中</title>
+        <link href="{{ secure_asset('css/check2.css') }}" rel="stylesheet">
+        <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    </head>
+    <body>
+        <br>
+        <img src="{{ asset('image/showroom.png') }}"></img>
+        <div class="button">
+                <a href="https://apps.apple.com/jp/app/showroom-%E3%83%A9%E3%82%A4%E3%83%96%E9%85%8D%E4%BF%A1%E3%81%AA%E3%82%89%E3%82%B7%E3%83%A7%E3%83%BC%E3%83%AB%E3%83%BC%E3%83%A0/id734256893" class="btn-square">公式ページで詳細をみる <i class="fas fa-external-link-alt"></i></a>
+        </div>
+        <p>公式ページへ移動しています。<br>詳細は公式ページをご確認下さい。</p>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -10,13 +10,27 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-// LP獲得用
+// LP獲得用(1)
 Route::get('job', function () {
     return view('gdn.check'); //提出用
     // return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=16355'); //獲得用(1)_直LP遷移
 });
 
+// LP獲得用(2)
+Route::get('live', function () {
+    return view('gdn.check2'); //提出用
+    // return redirect('./os'); //獲得用OS識別
+});
+
+//OS識別
+Route::get('os', 'AdController@os');
+
 // LP確認用
 Route::get('cccheck', function () {
     return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=16355'); //獲得用(1)_直LP遷移
+});
+
+// LP確認用2
+Route::get('cccheck2', function () {
+    return redirect('./os'); //獲得用OS識別
 });
