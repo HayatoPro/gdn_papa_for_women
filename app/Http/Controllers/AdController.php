187fc6931805d02e3c135f8ac044f152a1e775fe
EC2 Default User <ec2-user@ip-172-31-12-93.us-east-2.compute.internal>
2020-10-06 19:19:22 +0000

    Mm

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -10,8 +10,8 @@ class AdController extends Controller
     public function os(Request $request) {
         $user_agent =  $request->header('User-Agent');
         if ((strpos($user_agent, 'iPhone') !== false)) {
-            return redirect('https://a-trade.jp/redirect/fatey-multi?media=G16531'); //FATEY - Aトレード    
+            return redirect('https://preaf.jp/pa.do?s=v04031&o=47137&guid=ON'); //モモチャ - Preaf    
         }
-        return redirect('https://a-trade.jp/redirect/famu?media=G16531'); //FAMU - Aトレード
+        return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=16216'); //トリトモ - fam
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,21 +10,24 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-// LP獲得用(1)
-Route::get('job', function () {
-    return view('gdn.check'); //提出用
-    // return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=16355'); //獲得用(1)_直LP遷移
-});
+// // LP獲得用(1)
+// Route::get('job', function () {
+//     return view('gdn.check'); //提出用
+//     // return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=16355'); //獲得用(1)_直LP遷移
+// });
 
 // LP獲得用(2)
-Route::get('live', function () {
+Route::get('app', function () {
     return view('gdn.check2'); //提出用
+    // return view('gdn.adult'); //LP獲得用 - 男性向け
+    
     // return redirect('./os'); //獲得用OS識別
 });
 
 // LP確認用
 Route::get('cccheck', function () {
-    return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=16355'); //獲得用(1)_直LP遷移
+    return view('gdn.adult'); //LP獲得用 - 男性向け
+    // return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=16355'); //獲得用(1)_直LP遷移
 });
 
 // LP確認用2
