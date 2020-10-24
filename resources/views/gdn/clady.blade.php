f019d0ea7af9696ce64c387a944162c47a75897a
EC2 Default User <ec2-user@ip-172-31-12-93.us-east-2.compute.internal>
2020-10-24 21:36:04 +0000

    Mm

--- resources/views/gdn/clady.blade.php
+++ resources/views/gdn/clady.blade.php
@@ -30,11 +30,37 @@
                 <p>コスメや洋服、友だち付き合いや彼氏との旅行代などなど、何かと多い出費...。でも、あなたの周りにブランド物のコスメや洋服とか色んなものを次から次へと買っている友だち...いますよね？<br>今回はそのような方がどこでどのように稼いでいるのか、有名雑誌にも掲載されたスマホで出来る『非公開求人』を女性人気順に紹介したいと思いますっ!</p>
             </div>
             
+            <div class="rank1">
+                <h3>登録数No.1 - 在宅ワーク『ベリー』</h3>
+                <div class="rank1-img">
+                    <img src="{{ asset('image/cl_lp_2.jpg') }}"></img>
+                </div>
+                <div class="rank1-text">
+                    <p>この秋人気 & 登録数No.1は、在宅ワークの『ベリー』。条件面が非常に優れているのは勿論、有名雑誌に掲載されていたこともあり注目度は高くなっています。</p>
+                </div>
+                <div class="rank1-box">
+                    <span class="box-title">P O I N T <i class="far fa-hand-point-up"></i></span>
+                    <ul>
+                        <li>最大<span class="red">時給3,600円</span>！<br>▶▶▶ 平均でも2,500円以上</li>
+                        <li>嬉しい<span class="red">即払い制度</span>！<br>▶▶▶ 金欠の時だけ働く...でもOK</li>
+                        <li><span class="red">完全リモートワーク</span>対応！<br>▶▶▶︎ 電車内やカフェ、自宅でOK</li>
+                        <li>仕事内容は『悩み相談 & 話し相手』<br>▶▶▶ <span class="red">LINE感覚でチャットするだけ</span>でOK</li>
+                        <li>身バレ対策の徹底<br>▶▶▶ <span class="red">匿名OK & 専用アプリでの作業</span>なので安心</li>
+                    </ul>
+                </div>
+                <div class="rank1-button">
+                    <a href="#" class="btn-square-shadow"><i class="fab fa-apple"></i> 詳細はコチラをCHECK <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            
+            <div class="rank2">
+                <h3>テスト</h3>
+            </div>
+            
         </div>    
         
         <footer>
-            <!--ジャストアイディア-->
-            <!--LINE誘導してLINE@内で友達追加の案件訴求も実施? LP内でも勿論案件訴求するがあくまで控えめに？-->
+            
         </footer>
         <br>
         <br>
