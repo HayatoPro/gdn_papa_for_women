c1abcead2bdd3d66fb783a6e1e8cc10eadf89177
EC2 Default User <ec2-user@ip-172-31-12-93.us-east-2.compute.internal>
2020-10-25 07:46:48 +0000

    Mm

--- resources/views/gdn/clady.blade.php
+++ resources/views/gdn/clady.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>LP作成中</title>
+        <title>最新人気『非公開求人』特集！副業ナビ！</title>
         <link href="{{ secure_asset('css/clady.css') }}" rel="stylesheet">
         <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
     </head>
@@ -33,7 +33,7 @@
             <div class="rank1">
                 <h3>登録数No.1 - 在宅ワーク『ベリー』</h3>
                 <div class="rank1-img">
-                    <img src="{{ asset('image/cl_lp_2.jpg') }}"></img>
+                    <a href="https://a-trade.jp/redirect/berry?media=G16531" target="_blank"><img src="{{ asset('image/cl_lp_2.jpg') }}"></img></a>
                 </div>
                 <div class="rank1-text">
                     <p>この秋人気 & 登録数No.1は、在宅ワークの『ベリー』。条件面が非常に優れているのは勿論、有名雑誌に掲載されていたこともあり注目度は高くなっています。</p>
@@ -49,30 +49,71 @@
                     </ul>
                 </div>
                 <div class="rank1-button">
-                    <a href="#" class="btn-square-shadow"><i class="fab fa-apple"></i> 詳細はコチラをCHECK <i class="fas fa-arrow-circle-right"></i></a>
+                    <a href="https://a-trade.jp/redirect/berry?media=G16531" class="btn-square-shadow" target="_blank"><i class="fab fa-apple"></i> 詳細はコチラをCHECK <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
             
             <div class="rank2">
-                <h3>テスト</h3>
+                <h3>稼げるライブ配信『ビスコール』</h3>
+                <div class="rank2-img">
+                    <a href="https://a-trade.jp/redirect/biscor?media=G16531" target="blank"><img src="{{ asset('image/cl_lp_3.jpg') }}"></img></a>
+                </div>
+                <div class="rank2-text">
+                    <p>TikTokやYouTubeが話題になりがちな動画配信コンテンツ。ビスコールは従来の無料コンテンツをあげるだけではなく、時給を稼ぎながら配信できる新システムを導入！</p>
+                </div>
+                <div class="rank2-box">
+                    <span class="box-title">P O I N T <i class="far fa-hand-point-up"></i></span>
+                    <ul>
+                        <li>最大<span class="red">時給4,200円</span>！<br>▶▶▶ 視聴者からのプレゼントもGET！</li>
+                        <li>嬉しい<span class="red">即払い制度</span>！<br>▶▶▶ 金欠の時だけ配信...でもOK</li>
+                        <li>無名から<span class="red">有名インフルエンサーの仲間入り</span>！<br>▶▶▶︎ 一気に有名になれるチャンスも！ </li>
+                        <li>身バレ対策の徹底<br>▶▶▶ <span class="red">顔出し無しも可能</span>なので安心</li>
+                    </ul>
+                </div>
+                <div class="rank2-button">
+                    <a href="https://a-trade.jp/redirect/biscor?media=G16531" class="btn-square-shadow" target="_blank"><i class="fab fa-apple"></i> 詳細はコチラをCHECK <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
             </div>
             
+            <div class="rank3">
+                <h3>稼げるSNSアプリ『チャリン』</h3>
+                <div class="rank3-img">
+                    <a href="https://a-trade.jp/redirect/charin?media=G16531" target="_blank"><img src="{{ asset('image/cl_lp_4.jpg') }}"></img></a>
+                </div>
+                <div class="rank3-text">
+                    <p>新規SNSを活発化させる為、利用ユーザー同士でメッセージや電話するだけで稼げる仕組みを導入！毎日のスキマ時間でお小遣い稼ぎ、そして嬉しい翌日払いにも対応。</p>
+                </div>
+                <div class="rank3-box">
+                    <span class="box-title">P O I N T <i class="far fa-hand-point-up"></i></span>
+                    <ul>
+                        <li>最大<span class="red">時給4,200円</span>！<br>▶▶▶ 空いた1分からお小遣い稼ぎOK</li>
+                        <li>嬉しい<span class="red">即払い制度</span>！<br>▶▶▶ 24時間以内、翌日払いに対応</li>
+                        <li><span class="red">完全リモートワーク</span>対応！<br>▶▶▶︎ 電車内やカフェ、自宅でOK</li>
+                        <li>仕事内容は『SNSの利用』<br>▶▶▶ 新興コミュニティー内で<span class="red">ユーザー同士でメッセージをするだけ</span>でOK</li>
+                    </ul>
+                </div>
+                <div class="rank3-button">
+                    <a href="https://a-trade.jp/redirect/charin?media=G16531" class="btn-square-shadow" target="_blank"><i class="fab fa-apple"></i> 詳細はコチラをCHECK <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            
+            <div class="end1">
+                <h3>どんな人でも結局『合う合わない』はある！</h3>
+                <p>今回は独自調査の結果 特に人気ベスト3を紹介しましたが、結局のところあなたと稼ぐプラットフォームが合う合わないで収益は大きく変わります。</p>
+            </div>
+            
+            <div class="end2">
+                <h3>まずは、どれも試してみた方がいい！</h3>
+                <p>なので、『これが一番稼げそうだから』といった理由で一つのサービスに固執するのではなく、いろんなサービスをまずは試してみる。そして、その中から稼げそうなものをチャレンジしてみてください！<br>ちなみに、<span class="red">複数サービスの同時利用OK</span>です！</p>
+            </div>
+            
+            <div class="editor">
+                <img src="{{ asset('image/cl_lp_5.jpg') }}"></img>
+            </div>
         </div>    
         
         <footer>
-            
+            <img src="{{ asset('image/cl_lp_6.jpg') }}"></img>
         </footer>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
     </body>
 </html>
