bc3aad9268092055673b532a6bec66b3404cc514
EC2 Default User <ec2-user@ip-172-31-12-93.us-east-2.compute.internal>
2020-10-31 18:25:08 +0000

    Mm

--- resources/views/gdn/job.blade.php
+++ resources/views/gdn/job.blade.php
@@ -16,7 +16,7 @@
         
         <div class="big-body">
             <div class="body-top">
-                <a href="https://a-trade.jp/redirect/berry?media=G16531"><img src="{{ asset('image/lp_instagram_job.png') }}" class="wide"></a>
+                <a href="https://a-trade.jp/redirect/moa?media=G16531"><img src="{{ asset('image/lp_instagram_job.png') }}" class="wide"></a>
                 <img src="{{ asset('image/jobManga_1.png') }}" class="wide">
                 <img src="{{ asset('image/jobManga_2.png') }}" class="wide">
                 <h1><span>求人見つけたらラッキー!?</span></h1>
@@ -26,10 +26,10 @@
             <div class="big-body-main">
                 
                 <div class="body-main-3">
-                    <a href="https://a-trade.jp/redirect/berry?media=G16531"><img src="https://trading-ad.net/a-trade/banner/berry/02/01_600x500.png" class="wide"></a>
+                    <a href="https://a-trade.jp/redirect/moa?media=G16531"><img src="https://trading-ad.net/a-trade/banner/moa/02/01_600x500.png" class="wide"></a>
                     <p>今有名雑誌にも掲載されるなど、巷で話題になりつつある在宅スキマバイト。<br>お手持ちのスマホを使って、<span class="red">空いた1時間からお仕事可能</span>なのが魅力。また、しっかり報酬が高いのも特徴。</p>
                     <div class="body-button">
-                        <a href="https://a-trade.jp/redirect/berry?media=G16531" class="btn-square-shadow"><i class="fab fa-apple"></i> お仕事詳細はコチラ <i class="fas fa-arrow-circle-right"></i></a>
+                        <a href="https://a-trade.jp/redirect/moa?media=G16531" class="btn-square-shadow"><i class="fab fa-apple"></i> お仕事詳細はコチラ <i class="fas fa-arrow-circle-right"></i></a>
                         <p>※リンク切れの場合、募集停止している場合がございます。</p>
                     </div>
                     <br>
