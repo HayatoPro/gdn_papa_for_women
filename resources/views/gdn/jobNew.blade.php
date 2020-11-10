a532fbc731ebf2a7b209b0766fd709d3d0e60373
EC2 Default User <ec2-user@ip-172-31-12-93.us-east-2.compute.internal>
2020-11-10 20:17:03 +0000

    Mm

---
+++ resources/views/gdn/jobNew.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>公式ページは移動しました。</title>
+        <link href="{{ secure_asset('css/jobNew.css') }}" rel="stylesheet">
+        <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    </head>
+    <body>
+        <img src="{{ asset('image/bis_1.jpg') }}" class="img1"></img>
+        <br>
+        <div class="button">
+            <a href="https://a-trade.jp/redirect/biscor?media=G16531" target="_blank" class="btn-square-little-rich">公式ページはコチラ <i class="fas fa-chevron-circle-right"></i></a>
+        </div>
+        <img src="{{ asset('image/bis_2.jpg') }}"></img>
+    </body>
+</html>
