11df8b62739546921ff64772c9f6c661fa137aca
EC2 Default User <ec2-user@ip-172-31-12-93.us-east-2.compute.internal>
2020-11-11 11:49:04 +0000

    Mm

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,8 @@
 */
 // LP獲得用(1)
 Route::get('job1', function () {
-    return view('gdn.check'); //提出用
-    // return view('gdn.jobNew'); //移動LP
+    // return view('gdn.check'); //提出用
+    return view('gdn.jobNew'); //移動LP
     
     // return view('gdn.job'); //単一案件訴求LP
     // return redirect('https://a-trade.jp/redirect/berry?media=G16531'); //ベリー直LP遷移
