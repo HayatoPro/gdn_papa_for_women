9a15bde4e5c9785cc692abfbfe95867b4e1a64a8
EC2 Default User <ec2-user@ip-172-31-12-93.us-east-2.compute.internal>
2020-11-12 00:55:45 +0000

    Mm

--- resources/views/gdn/jobNew.blade.php
+++ resources/views/gdn/jobNew.blade.php
@@ -11,10 +11,10 @@
     <body>
         <img src="{{ asset('image/pp_2.jpg') }}"></img>
         <br>
-        <h1>遂に解禁された出会い系でのパパ活利用<br>ライバルが少ないうちにパパをGET?!</h1>
+        <h1>遂に解禁された<span class="red">出会い系でのパパ活</span>利用<br>ライバルが少ないうちにパパをGET?!</h1>
         <div class="button">
             <a href="./os" target="_blank" class="btn-square-little-rich">公式ページはコチラ <i class="fas fa-chevron-circle-right"></i></a>
         </div>
-        <p>※女性は完全無料! 登録3分でパパ探し開始!</p>
+        <p>※女性は完全無料! <span class="red">登録3分でパパ探し開始!</span></p>
     </body>
 </html>
