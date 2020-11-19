95dcd93392ce5bdb7ab7d2f238ef118f34218068
EC2 Default User <ec2-user@ip-172-31-12-93.us-east-2.compute.internal>
2020-11-19 19:11:11 +0000

    Mm

--- resources/views/gdn/jobNew.blade.php
+++ resources/views/gdn/jobNew.blade.php
@@ -4,17 +4,16 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>公式ページは移動しました。</title>
+        <title>公式ページヘ移動してください。</title>
         <link href="{{ secure_asset('css/jobNew.css') }}" rel="stylesheet">
         <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
     </head>
     <body>
-        <img src="{{ asset('image/pp_2.jpg') }}"></img>
-        <br>
         <h1>遂に解禁された<span class="red">出会い系でのパパ活</span>利用<br>ライバルが少ないうちにパパをGET?!</h1>
         <div class="button">
             <a href="./os" target="_blank" class="btn-square-little-rich">公式ページはコチラ <i class="fas fa-chevron-circle-right"></i></a>
         </div>
         <p>※女性は完全無料! <span class="red">登録3分でパパ探し開始!</span></p>
+        <img src="{{ asset('image/pp_2.jpg') }}"></img>
     </body>
 </html>
