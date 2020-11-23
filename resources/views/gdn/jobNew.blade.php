9cc19c2b9cc890cc424a3cda3a453c3208a5a39c
EC2 Default User <ec2-user@ip-172-31-12-93.us-east-2.compute.internal>
2020-11-23 23:42:31 +0000

    mm

--- resources/views/gdn/jobNew.blade.php
+++ resources/views/gdn/jobNew.blade.php
@@ -9,11 +9,12 @@
         <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
     </head>
     <body>
+        <img src="{{ asset('image/pp_2.jpg') }}"></img>
         <h1>遂に解禁された<span class="red">出会い系でのパパ活</span>利用<br>ライバルが少ないうちにパパをGET?!</h1>
         <div class="button">
             <a href="./os" target="_blank" class="btn-square-little-rich">公式ページはコチラ <i class="fas fa-chevron-circle-right"></i></a>
         </div>
         <p>※女性は完全無料! <span class="red">登録3分でパパ探し開始!</span></p>
-        <img src="{{ asset('image/pp_2.jpg') }}"></img>
+        <br>
     </body>
 </html>
