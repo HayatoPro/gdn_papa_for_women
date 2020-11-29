2cccba920475618759bc94dc52e1ef1834175afb
EC2 Default User <ec2-user@ip-172-31-12-93.us-east-2.compute.internal>
2020-11-29 19:13:09 +0000

    mm

--- resources/views/gdn/jobNew.blade.php
+++ resources/views/gdn/jobNew.blade.php
@@ -10,11 +10,13 @@
     </head>
     <body>
         <img src="{{ asset('image/pp_2.jpg') }}"></img>
-        <h1>遂に解禁された<span class="red">出会い系でのパパ活</span>利用<br>ライバルが少ないうちにパパをGET?!</h1>
+        <!--<h1>遂に解禁された<span class="red">出会い系でのパパ活</span>利用<br>ライバルが少ないうちにパパをGET?!</h1>-->
+        <h1>遂に解禁された<span class="red">新情報とは!?</span><br>ライバルが少ないうちにパパをGET?!</h1>
         <div class="button">
-            <a href="./os" target="_blank" class="btn-square-little-rich">公式ページはコチラ <i class="fas fa-chevron-circle-right"></i></a>
+            <a href="https://lin.ee/nvxfLzP" target="_blank" class="btn-square-little-rich">『公式LINE』で情報GET! <i class="fas fa-chevron-circle-right"></i></a>
         </div>
-        <p>※女性は完全無料! <span class="red">登録3分でパパ探し開始!</span></p>
+        <!--<p>※女性は完全無料! <span class="red">登録3分でパパ探し開始!</span></p>-->
+        <p>※<span class="green">LINE友達追加</span>で、今なら<br><span class="red">今、最も初心者に稼ぎやすいパパ活アプリ</span>を紹介中!</p>
         <!--<br>-->
         <img src="{{ asset('image/p_manga_1.jpg') }}"></img>
         <img src="{{ asset('image/p_manga_2.jpg') }}"></img>
@@ -22,9 +24,11 @@
         <img src="{{ asset('image/p_manga_4.jpg') }}"></img>
         <h1>初心者でも大丈夫! <span class="red">むしろ初心者向け！</span><br>パパ見つけやすさに定評あり!</h1>
         <div class="button">
-            <a href="./os" target="_blank" class="btn-square-little-rich">公式ページはコチラ <i class="fas fa-chevron-circle-right"></i></a>
+            <!--<a href="./os" target="_blank" class="btn-square-little-rich">公式ページはコチラ <i class="fas fa-chevron-circle-right"></i></a>-->
+            <a href="https://lin.ee/nvxfLzP" target="_blank" class="btn-square-little-rich">『公式LINE』で情報GET! <i class="fas fa-chevron-circle-right"></i></a>
         </div>
-        <p>※女性は完全無料! <span class="red">登録3分でパパ探し開始!</span></p>
+        <!--<p>※女性は完全無料! <span class="red">登録3分でパパ探し開始!</span></p>-->
+        <p>※<span class="green">LINE友達追加</span>で、今なら<br><span class="red">今、最も初心者に稼ぎやすいパパ活アプリ</span>を紹介中!</p>
         <br>
     </body>
 </html>
