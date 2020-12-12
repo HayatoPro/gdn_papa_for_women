f8acd4853f0c82e31dc1bc1d3427a3b96288f56d
EC2 Default User <ec2-user@ip-172-31-12-93.us-east-2.compute.internal>
2020-12-12 00:38:16 +0000

    Mm

--- resources/views/gdn/jobNew.blade.php
+++ resources/views/gdn/jobNew.blade.php
@@ -16,7 +16,7 @@
             <a href="https://lin.ee/nvxfLzP" target="_blank" class="btn-square-little-rich">『公式LINE』で情報GET! <i class="fas fa-chevron-circle-right"></i></a>
         </div>
         <!--<p>※女性は完全無料! <span class="red">登録3分でパパ探し開始!</span></p>-->
-        <p>※<span class="green">LINE友達追加</span>で、今なら<br><span class="red">今、最も初心者に稼ぎやすいパパ活アプリ</span>を紹介中!</p>
+        <p>※<span class="green">LINE友達追加</span>で、今なら<br><span class="red">今、最も初心者に稼ぎやすいパパ活</span>を紹介中!</p>
         <!--<br>-->
         <img src="{{ asset('image/p_manga_1.jpg') }}"></img>
         <img src="{{ asset('image/p_manga_2.jpg') }}"></img>
@@ -28,7 +28,7 @@
             <a href="https://lin.ee/nvxfLzP" target="_blank" class="btn-square-little-rich">『公式LINE』で情報GET! <i class="fas fa-chevron-circle-right"></i></a>
         </div>
         <!--<p>※女性は完全無料! <span class="red">登録3分でパパ探し開始!</span></p>-->
-        <p>※<span class="green">LINE友達追加</span>で、今なら<br><span class="red">今、最も初心者に稼ぎやすいパパ活アプリ</span>を紹介中!</p>
+        <p>※<span class="green">LINE友達追加</span>で、今なら<br><span class="red">今、最も初心者に稼ぎやすいパパ活</span>を紹介中!</p>
         <br>
     </body>
 </html>
